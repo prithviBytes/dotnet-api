2f1aa2196d3bb5a91b57ab68e21ecc1bb79fbbe6
prithviBytes <prithvisuvarna56@gmail.com>
2021-08-30 12:46:10 +0530

    add: automapper

--- API/Extentions/ApplicationServiceExtention.cs
+++ API/Extentions/ApplicationServiceExtention.cs
@@ -1,6 +1,7 @@
 using API.Data;
 using API.Interfaces;
 using API.Services;
+using API.Helpers;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -13,6 +14,7 @@ namespace API.Extentions
         {
             services.AddScoped<ITokenService, TokenService>();
             services.AddScoped<IUserRepository, UserRepository>();
+            services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
             services.AddDbContext<DataContext>(options =>
             {
                 options.UseSqlite(config.GetConnectionString("Default"));
