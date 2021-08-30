1cdd74cbeca9871e47e9ac29e1bb8da0fc52dab9
prithviBytes <prithvisuvarna56@gmail.com>
2021-08-30 12:06:12 +0530

    add: member and photo dto

---
+++ API/DTOs/MemberDto.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using API.Entities;
+
+namespace API.DTOs
+{
+    public class MemberDto
+    {
+        public int Id { get; set; }
+        public string UserName { get    ; set; }
+        public DateTime DateOfBirth { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime LastActive { get; set; } = DateTime.Now;
+        public string KnownAs { get; set; }
+        public string Gender { get; set; }
+        public string Introduction { get; set; }
+        public string LookingFor { get; set; }
+        public string  Interests { get; set; }
+        public string City { get; set; }
+        public string  Country { get; set; }
+        public ICollection<PhotoDto> Photos { get; set; }
+    }
+}
