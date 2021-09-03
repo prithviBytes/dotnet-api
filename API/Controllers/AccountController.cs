ce75d2bf7703cdf36fa5e22b7f19863b5113ac91
prithviBytes <prithvisuvarna56@gmail.com>
2021-09-03 22:35:49 +0530

    add: delete photo endpoint

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
@@ -46,7 +47,9 @@ namespace API.Controllers
         [HttpPost("login")]
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto){
 
-            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
+            var user = await _context.Users.
+                Include(p => p.Photos).
+                SingleOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
 
             if(user == null) return Unauthorized("Invalid Username");
 
@@ -60,7 +63,8 @@ namespace API.Controllers
             }
             return new UserDto{
                 Username = user.UserName,
-                Token = _token.CreateToken(user)
+                Token = _token.CreateToken(user),
+                PhotoUrl = user.Photos.FirstOrDefault(photo => photo.IsMain)?.Url
             };
         }
     }
